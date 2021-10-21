217966db17e18f28a643461b3d6cd01b9a7488ff
Gaspar Morales <gasparmorales1@gmail.com>
2021-10-20 23:22:03 -0300

    eliminar usuario

--- controller/index.php
+++ controller/index.php
@@ -1,19 +1,23 @@
 <?php
-require "../model/conexion.php";
-require "../model/persona.php";
+//require "../model/conexion.php";
+require "../model/PHP/conexion-docker.php";
+require "../model/PHP/persona.php";
 
 $persona = new Persona;
 $listaPersonas = $persona->listarPersona();
-foreach ($listaPersonas as $row) {
-    echo $row["ID"] . " ";
-    echo $row["Cedula"] . " ";
-    echo $row["Nombre"] . " ";
-    echo $row["Apellido"] . " ";
-    echo $row["Direccion"] . " ";
-    echo $row["Telefono"] . " ";
-    echo $row["Email"] . " ";
-    echo "<br>";
+
+if (isset($_POST["modificar"])) {
+  echo "modificar";
 }
+if (isset($_POST["eliminar"])) {
+  $id = $_POST["id"];
+  $persona->setID($id);
+  $persona->eliminarPersona();
+  $listaPersonas = $persona->listarPersona();
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -23,35 +27,64 @@ foreach ($listaPersonas as $row) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../view/styles.css">
 
     <title>Inicio</title>
 </head>
 <body>
-    <nav>
+  <?php
+  require_once "../view/templateNavbar.php";
+  if ($listaPersonas) {
 
-    </nav>
+    ?>
+    <table class="table table-bordered table-info">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">CI</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">Direccion</th>
+          <th scope="col">Telefono</th>
+          <th scope="col">Email</th>
+          <th scope="col">Accion</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        
+        foreach ($listaPersonas as $row) {
+          echo "<tr>";
+          echo "<td>" . $row['ID'] . " </td> ";
+          echo "<td>" . $row['Cedula'] . " </td> ";
+          echo "<td>" . $row['Nombre'] . " </td> ";
+          echo "<td>" . $row['Apellido'] . " </td> ";
+          echo "<td>" . $row['Direccion'] . " </td> ";
+          echo "<td>" . $row['Telefono'] . " </td> ";
+          echo "<td>" . $row['Email'] . " </td> ";
+          ?>
+          <td>
+            <form action="" method="post">
+              <input type="hidden" name="id" value="<?php echo $row['ID']; ?>">
+              <button type="submit" name="modificar" class="btn btn-primary">Modificar</button>
+              <button type="submit" name="eliminar" class="btn btn-danger">Eliminar</button>
+            </form>
+          </td>
+          <?php
+          echo "</tr>";
+        }
+        
+        ?>
+      </tbody>
+    </table>
+    <?php
+  }else{
+    echo "<h1>No hay registro de clientes </h1>";
+  }
+  ?>
 
-    <table class="table table-bordered table-info margenes">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">CI</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Direccion</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Mail</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
 </body>
 </html>
 
+<script src="../model/JS/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
---
+++ view/templateNavbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href=".">ABML</a>
+    <div class="collapse navbar-collapse flex-row-reverse transition" id="navbarNav">
+      <ul class="navbar-nav">
+
+        <li class="nav-item mx-3">
+          <div class="dropdown">
+            <a class="btn navbarItem  Brounded navBold text-white" href="agregar.php" role="button">
+              <i class="fas fa-users mx-2"></i>
+              Insertar Persona
+            </a>
+          </div>
+        </li>
+
+        <li class="nav-item mx-3">
+          <div class="dropdown">
+            <a class="btn navbarItem  Brounded navBold text-white" href="." role="button">
+              <i class="fas fa-users mx-2"></i>
+              Listado de Personas
+            </a>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
