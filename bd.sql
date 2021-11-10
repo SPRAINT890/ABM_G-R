0fcbbfa20aca6cc4ec287ca3b5c6ff6378af2578
roglattli <roglattli@gmail.com>
2021-11-10 14:39:06 -0300

    Aumente los caracteres de direccion a 60 en la BD

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@ ID int auto_increment primary key,
  Cedula varchar(9) not null unique key,
  Nombre varchar (20),
  Apellido varchar(20),
- Direccion varchar(25),
+ Direccion varchar(60),
  Telefono varchar(9),
  Email varchar(25)
 );
